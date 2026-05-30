8db72eecaca4f052a766d7679cbff7c1b71b767d
Ribyn <robinmeow97@gmail.com>
2026-05-30 14:32:46 +0200

    enable bashls for other shell files

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -357,7 +357,9 @@ require("lazy").setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        ["bashls"] = {},
+        ["bashls"] = {
+          filetypes = { "bash", "csh", "ksh", "sh", "zsh" },
+        },
         ["ts_ls"] = {}, -- https://github.com/pmizio/typescript-tools.nvim can be considered when more speed is required
         ["eslint"] = {},
         ["shellcheck"] = {},
--- zshrc
+++ zshrc
@@ -1,4 +1,5 @@
 # vim: ft=zsh
+# shellcheck disable=SC2148
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
