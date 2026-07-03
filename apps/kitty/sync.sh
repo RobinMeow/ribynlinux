1f212cd82dd700b1ce076d2ad2828d7e6600a4f9
Ribyn <robinmeow97@gmail.com>
2026-07-03 12:02:46 +0200

    kitty: disable wsl throttling

--- apps/kitty/sync.sh
+++ apps/kitty/sync.sh
@@ -4,19 +4,21 @@ set -euo pipefail
 mkdir -p "$HOME/.config/kitty"
 cp -r "$RIBYNS_ENV/apps/kitty/config/"* "$HOME/.config/kitty/"
 
+. "$RIBYNS_ENV/lib/utils.sh"
+warn "Kitty: WSL specific config is disabled."
 # NOTE: wsl is not as fast as native.
 # so im using a throttled kitty perf
-source "$RIBYNS_ENV/lib/detect_env.sh"
-detect_env
-if [[ "$OS_TYPE" == "wsl" ]]; then
-	cat >>"$HOME/.config/kitty/kitty.conf" <<EOF
-
-# WSL Specific Changes
-font_size 11
-repaint_delay 100
-input_delay 5
-sync_to_monitor no
-EOF
-
-	info "Kitty: WSL specific config has been appended."
-fi
+# source "$RIBYNS_ENV/lib/detect_env.sh"
+# detect_env
+# if [[ "$OS_TYPE" == "wsl" ]]; then
+# 	cat >>"$HOME/.config/kitty/kitty.conf" <<EOF
+#
+# # WSL Specific Changes
+# font_size 11
+# repaint_delay 100
+# input_delay 5
+# sync_to_monitor no
+# EOF
+#
+# 	info "Kitty: WSL specific config has been appended."
+# fi
