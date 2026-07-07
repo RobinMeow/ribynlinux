ed58f850030fc7b8b5dc8fc9b8c1c5d74f7446d5
Ribyn <robinmeow97@gmail.com>
2026-07-07 16:05:03 +0200

    nvim: virtual text while debugging

--- apps/nvim/config/lua/plugins/debug.lua
+++ apps/nvim/config/lua/plugins/debug.lua
@@ -2,7 +2,7 @@
 --
 -- dotnet in easy-dotnet https://github.com/GustavEikaas/easy-dotnet.nvim/blob/main/docs/debugging.md
 
--- show virtual text of values https://github.com/theHamsta/nvim-dap-virtual-text?utm_source=chatgpt.com
+-- show virtual text of values https://github.com/theHamsta/nvim-dap-virtual-text
 
 return {
   enabled = true,
@@ -15,6 +15,46 @@ return {
     "jay-babu/mason-nvim-dap.nvim",
 
     -- 'leoluz/nvim-dap-go', -- GO debugger
+
+    {
+      "theHamsta/nvim-dap-virtual-text",
+      build = ":TSUpdate",
+      config = function()
+        require("nvim-dap-virtual-text").setup({
+          enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+          highlight_changed_variables = false, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+          highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+          show_stop_reason = true, -- show stop reason when stopped for exceptions
+          commented = false, -- prefix virtual text with comment string
+          only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+          all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+          clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
+          --- A callback that determines how a variable is displayed or whether it should be omitted
+          --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
+          --- @param buf number
+          --- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
+          --- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
+          --- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
+          --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
+          display_callback = function(variable, buf, stackframe, node, options)
+            -- by default, strip out new line characters
+            if options.virt_text_pos == "inline" then
+              return " = " .. variable.value:gsub("%s+", " ")
+            else
+              return variable.name .. " = " .. variable.value:gsub("%s+", " ")
+            end
+          end,
+          -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
+          -- virt_text_pos = vim.fn.has("nvim-0.10") == 1 and "inline" or "eol",
+          virt_text_pos = "eol",
+
+          -- disable experimental features (as of 2026-07-07)
+          all_frames = false,
+          virt_lines = false,
+          virt_text_win_col = nil,
+        })
+      end,
+    },
   },
 	-- stylua: ignore
   keys = {
