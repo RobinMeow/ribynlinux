3b8abc8025654956885c986a84b531d502a3ce01
Ribyn <robinmeow97@gmail.com>
2026-07-21 13:01:00 +0200

    godot: remove 4.5 configurations

--- apps/godot/sync.sh
+++ apps/godot/sync.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 
 mkdir -p "$RIBYN_ROOT/.backup/godot"
 echo "Backing up old godot files in $RIBYN_ROOT/.backup/godot"
-cp "$HOME/.config/godot/editor_settings-4.5.tres" "$RIBYN_ROOT/.backup/godot"
 cp "$HOME/.config/godot/editor_settings-4.7.tres" "$RIBYN_ROOT/.backup/godot"
 
-cp "$RIBYN_ROOT/apps/godot/config/editor_settings-4.5.tres" "$HOME/.config/godot/editor_settings-4.5.tres"
 cp "$RIBYN_ROOT/apps/godot/config/editor_settings-4.7.tres" "$HOME/.config/godot/editor_settings-4.7.tres"
