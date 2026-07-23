bb0016a945a1dd01484f8372ba8ecf7f67c24715
Ribyn <robinmeow97@gmail.com>
2026-07-23 18:47:50 +0200

    godot: save half_screen editor layout and sync it

--- apps/godot/sync.sh
+++ apps/godot/sync.sh
@@ -6,3 +6,4 @@ echo "Backing up old godot files in $RIBYN_ROOT/.backup/godot"
 cp "$HOME/.config/godot/editor_settings-4.7.tres" "$RIBYN_ROOT/.backup/godot"
 
 cp "$RIBYN_ROOT/apps/godot/config/editor_settings-4.7.tres" "$HOME/.config/godot/editor_settings-4.7.tres"
+cp "$RIBYN_ROOT/apps/godot/config/editor_layouts.cfg" "$HOME/.config/godot/editor_layouts.cfg"
