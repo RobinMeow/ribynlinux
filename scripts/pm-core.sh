0a38b8339e5848325d5351539a385724f074d584
Ribyn <robinmeow97@gmail.com>
2026-08-02 18:06:32 +0200

    pm-core.sh: add wireplumber and rsync

--- scripts/pm-core.sh
+++ scripts/pm-core.sh
@@ -10,6 +10,7 @@ source "$RIBYN_ROOT/lib/run_on_distro.sh"
 run_on_arch <<'EOF'
 	sudo pacman -S --needed --noconfirm \
 		base-devel \
+		wireplumber \
 		gawk \
 		git \
 		curl \
@@ -18,6 +19,7 @@ run_on_arch <<'EOF'
 		vi \
 		vim \
 		go \
+		rsync \
 		tree-sitter-cli \
 		unzip \
 		xclip \
@@ -56,6 +58,7 @@ EOF
 run_on_fedora <<'EOF'
 	sudo dnf install -y \
 		@development-tools \
+		wireplumber \
 		git \
 		gawk \
 		curl \
@@ -64,6 +67,7 @@ run_on_fedora <<'EOF'
 		vi \
 		vim \
 		golang \
+		rsync \
 		tree-sitter-cli \
 		unzip \
 		xclip \
