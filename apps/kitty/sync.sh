b41052e72c68fe2b02b7934aa1a56586477c556b
Ribyn <robinmeow97@gmail.com>
2026-08-03 07:56:15 +0200

    kitty: re-enable wsl specific changes

--- apps/kitty/sync.sh
+++ apps/kitty/sync.sh
@@ -8,17 +8,24 @@ cp -r "$RIBYN_ROOT/apps/kitty/config/"* "$HOME/.config/kitty/"
 warn "Kitty: WSL specific config is disabled."
 # NOTE: wsl is not as fast as native.
 # so im using a throttled kitty perf
-# source "$RIBYN_ROOT/lib/detect_env.sh"
-# detect_env
-# if [[ "$OS_TYPE" == "wsl" ]]; then
-# 	cat >>"$HOME/.config/kitty/kitty.conf" <<EOF
-#
-# # WSL Specific Changes
+source "$RIBYN_ROOT/lib/detect_env.sh"
+detect_env
+if [[ "$OS_TYPE" == "wsl" ]]; then
+	cat >>"$HOME/.config/kitty/kitty.conf" <<EOF
+
+# WSL Specific Changes
+background_image $HOME/.config/kitty/background.jpg
+background_opacity 0
+background_image_layout scaled
+# darken the bg
+background black
+background_tint 0.99
+
 # font_size 11
 # repaint_delay 100
 # input_delay 5
 # sync_to_monitor no
-# EOF
-#
-# 	info "Kitty: WSL specific config has been appended."
-# fi
+EOF
+
+	info "Kitty: WSL specific config has been appended."
+fi
