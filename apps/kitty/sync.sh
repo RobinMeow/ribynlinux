78a286bd2659818de99bfbd457fe783af93477bb
Ribyn <robinmeow97@gmail.com>
2026-08-05 14:13:32 +0200

    kitty: swap background_image to hypr_chan

--- apps/kitty/sync.sh
+++ apps/kitty/sync.sh
@@ -6,21 +6,22 @@ cp -r "$RIBYN_ROOT/apps/kitty/config/"* "$HOME/.config/kitty/"
 
 . "$RIBYN_ROOT/lib/utils.sh"
 warn "Kitty: WSL specific config is disabled."
-# NOTE: wsl is not as fast as native.
-# so im using a throttled kitty perf
-source "$RIBYN_ROOT/lib/detect_env.sh"
+
+. "$RIBYN_ROOT/lib/detect_env.sh"
 detect_env
+
 if [[ "$OS_TYPE" == "wsl" ]]; then
 	cat >>"$HOME/.config/kitty/kitty.conf" <<EOF
 
 # WSL Specific Changes
-background_image $HOME/.config/kitty/background.jpg
+background_image $HOME/.config/kitty/background.png
 background_opacity 0
 background_image_layout scaled
 # darken the bg
 background black
 background_tint 0.99
 
+# WARN: comment in to throttle rendering to save a bit of performance
 # font_size 11
 # repaint_delay 100
 # input_delay 5
