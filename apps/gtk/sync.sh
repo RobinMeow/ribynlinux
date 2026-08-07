24430427f32e0abc21ba38f09aa962e9072541a0
Ribyn <robinmeow97@gmail.com>
2026-08-07 22:10:39 +0200

    gtk: set dtk prefered theme to dark theme using several methods

---
+++ apps/gtk/sync.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+. "$RIBYN_ROOT/lib/utils.sh" # ribyn_rsync
+
+mkdir -p "$HOME/.config/gtk-3.0/"
+ribyn_rsync \
+	"$RIBYN_ROOT/apps/gtk/config/" \
+	"$HOME/.config/gtk-3.0/"
+
+mkdir -p "$HOME/.config/gtk-4.0/"
+ribyn_rsync \
+	"$RIBYN_ROOT/apps/gtk/config/" \
+	"$HOME/.config/gtk-4.0/"
+
+# Set GTK theme and color scheme via gsettings
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
