8e27509cc40ef2cb41d160fddac4186ec4d52f26
Ribyn <robinmeow97@gmail.com>
2026-08-08 19:45:18 +0200

    hypr: add build_hyprmoncfg_from_source.sh

---
+++ apps/hypr/build_hyprmoncfg_from_source.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# https://github.com/crmne/hyprmoncfg/commits/main/
+
+source "$RIBYN_ROOT/lib/utils.sh"
+source "$RIBYN_ROOT/lib/run_on_distro.sh"
+
+run_on_arch sudo pacman -S --needed --noconfirm \
+	go
+
+run_on_fedora sudo dnf install -y \
+	go
+
+mkdir -p "$HOME/.local/share/ribyn/"
+dest="$HOME/.local/share/ribyn/hyprmoncfg"
+(
+	git clone --depth 1 "https://github.com/crmne/hyprmoncfg.git" "$dest"
+	cd "$dest"
+	go build -o bin/hyprmoncfg ./cmd/hyprmoncfg
+	go build -o bin/hyprmoncfgd ./cmd/hyprmoncfgd
+	install -Dm755 bin/hyprmoncfg ~/.local/bin/hyprmoncfg
+	install -Dm755 bin/hyprmoncfgd ~/.local/bin/hyprmoncfgd
+)
