22627697a3199ccaf1d597bb4d648c9e1f611434
Ribyn <robinmeow97@gmail.com>
2026-08-09 15:11:05 +0200

    apps: add brave

---
+++ apps/brave/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+. "$RIBYN_ROOT/lib/run_on_distro.sh"
+
+# arch
+run_on_arch yay -Sy --needed --noconfirm \
+	brave-bin
+
+# fedora
+run_on_fedora sudo dnf install -y \
+	dnf-plugins-core
+
+repofile="https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo"
+run_on_fedora sudo dnf config-manager addrepo --from-repofile="$repofile"
+
+run_on_fedora sudo dnf install -y \
+	brave-browser
