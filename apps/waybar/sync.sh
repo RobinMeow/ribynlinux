92b74b4565f05476b6eaa81c73933fad9bad6a1d
Ribyn <robinmeow97@gmail.com>
2026-08-10 18:05:01 +0200

    waybar: refac sync.sh to use ribyn_rsync

--- apps/waybar/sync.sh
+++ apps/waybar/sync.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+. "$RIBYN_ROOT/lib/utils.sh"
+
 mkdir -p "$HOME/.config/waybar"
-cp -r "$RIBYN_ROOT/apps/config/waybar/"* "$HOME/.config/waybar/"
+ribyn_rsync \
+	"$RIBYN_ROOT/apps/waybar/config/" \
+	"$HOME/.config/waybar/"
