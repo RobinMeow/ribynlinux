61859296b8058e9ce99421795400c182a55c9311
Ribyn <robinmeow97@gmail.com>
2026-08-11 16:34:15 +0200

    installers: add no tofu

---
+++ installers/no_tofu.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+. "$RIBYN_ROOT/lib/utils.sh"
+. "$RIBYN_ROOT/lib/run_on_distro.sh"
+
+mkdir -p "$HOME/.local/share/ribyn/no_tofu"
+
+first_time_install="$HOME/.local/share/ribyn/no_tofu/first_time_install"
+if [[ ! -f "$first_time_install" ]]; then
+	touch "$first_time_install"
+
+	run_on_arch sudo pacman -S --needed --noconfirm \
+		noto-fonts \
+		noto-fonts-{cjk,emoji,extra}
+
+	run_on_fedora sudo dnf install -y \
+		noto-fonts-*
+
+	# rebuild font cache
+	sudo fc-cache -fv
+	warn "[no tofu] might requires a reboot to take effect"
+else
+	info "[no tofu] already installed. skipping."
+fi
