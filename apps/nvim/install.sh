75bc2a835ff6a359aab807f370e0fa781bfb643b
Ribyn <robinmeow97@gmail.com>
2026-08-12 17:33:35 +0200

    nvim: add missing tree-sitter-cli

--- apps/nvim/install.sh
+++ apps/nvim/install.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+. "$RIBYN_ROOT/lib/run_on_distro.sh"
+
+run_on_arch sudo pacman -S --needed --noconfirm \
+	tree-sitter-cli
+
+run_on_fedora sudo dnf install -y \
+	tree-sitter-cli
+
 "$RIBYN_ROOT/apps/nvim/build-from-source.sh"
 "$RIBYN_ROOT/apps/nvim/sync.sh"
