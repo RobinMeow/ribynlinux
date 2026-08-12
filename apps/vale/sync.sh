f122392ca918f8351ac933e7c14b421609696be3
Ribyn <robinmeow97@gmail.com>
2026-08-12 23:26:14 +0200

    vale: only redownload styles on sync when the vale.ini hash changed for faster syncs

--- apps/vale/sync.sh
+++ apps/vale/sync.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+. "$RIBYN_ROOT/lib/utils.sh"
+
 mkdir -p "$HOME/.config/vale"
-cp "$RIBYN_ROOT/apps/vale/config/"* "$HOME/.config/vale/"
-vale --config "$HOME/.config/vale/vale.ini" sync
+
+if [[ -f "$HOME/.config/vale/vale.ini" ]]; then
+	hash_before_sync=$(cksum -a sha256 --untagged "$HOME/.config/vale/vale.ini")
+
+	ribyn_rsync \
+		"$RIBYN_ROOT/apps/vale/config/vale.ini" \
+		"$HOME/.config/vale/vale.ini"
+
+	if [[ "$(cksum -a sha256 --untagged "$HOME/.config/vale/vale.ini")" != "$hash_before_sync" ]]; then
+		vale --config "$HOME/.config/vale/vale.ini" sync
+	fi
+else
+	ribyn_rsync \
+		"$RIBYN_ROOT/apps/vale/config/vale.ini" \
+		"$HOME/.config/vale/vale.ini"
+
+	vale --config "$HOME/.config/vale/vale.ini" sync
+fi
