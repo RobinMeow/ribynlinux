9361bf4bb676a1b4a7f7948273eb2799f19b8789
Ribyn <robinmeow97@gmail.com>
2026-08-12 18:09:56 +0200

    nvim: add missing dep npm to install.sh

--- apps/nvim/install.sh
+++ apps/nvim/install.sh
@@ -4,10 +4,14 @@ set -euo pipefail
 . "$RIBYN_ROOT/lib/run_on_distro.sh"
 
 run_on_arch sudo pacman -S --needed --noconfirm \
-	tree-sitter-cli
+	tree-sitter-cli \
+	npm
+# treesitter plugin depends on tree-sitter-cli
+# mason depends on npm
 
 run_on_fedora sudo dnf install -y \
-	tree-sitter-cli
+	tree-sitter-cli \
+	npm
 
 "$RIBYN_ROOT/apps/nvim/build-from-source.sh"
 "$RIBYN_ROOT/apps/nvim/sync.sh"
