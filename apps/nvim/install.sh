21a78fc74354ae30f263409e0d85d479612bfdcc
Ribyn <robinmeow97@gmail.com>
2026-08-12 18:19:20 +0200

    fix: move pm-webdev to pm-core which was no longer called which had the missing deps for nvim

--- apps/nvim/install.sh
+++ apps/nvim/install.sh
@@ -8,6 +8,9 @@ run_on_arch sudo pacman -S --needed --noconfirm \
 	npm
 # treesitter plugin depends on tree-sitter-cli
 # mason depends on npm
+# mason depends on dotnet for csharpier
+# TODO: [ERROR Wed Aug 12 16:12:16 2026] ...al/share/nvim/lazy/mason.nvim/lua/mason-core/process.lua:226:
+# Failed to spawn process. cmd="dotnet", err="ENOENT: no such file or directory"
 
 run_on_fedora sudo dnf install -y \
 	tree-sitter-cli \
--- scripts/pm-core.sh
+++ scripts/pm-core.sh
@@ -9,102 +9,117 @@ source "$RIBYN_ROOT/lib/run_on_distro.sh"
 #
 # INFO: just silencing nvim warnings with these: php composer jdk-openjdk julia
 
-run_on_arch <<'EOF'
-	sudo pacman -S --needed --noconfirm \
-		base-devel \
-		wireplumber \
-		gawk \
-		git \
-		curl \
-		zsh \
-		eza \
-		vi \
-		vim \
-		go \
-		rsync \
-		tree-sitter-cli \
-		unzip \
-		xclip \
-		wl-clipboard \
-		openssh \
-		navi \
-		fastfetch \
-		bc \
-		ncdu \
-		cargo \
-		man-db \
-		man-pages \
-		tealdeer \
-		vlc \
-		vlc-plugins-all \
-		lnav \
-		tree \
-		btop \
-		translate-shell \
-		tokei \
-		ffmpeg \
-		7zip \
-		ripgrep \
-		glow \
-		mpv \
-		chafa \
-		fd \
-		bluetui \
-		gource \
-		words \
-		detox \
-		git-lfs \
-		php composer jdk-openjdk julia \
-		wev
-EOF
+run_on_arch sudo pacman -S --needed --noconfirm \
+	base-devel \
+	wireplumber \
+	gawk \
+	git \
+	curl \
+	zsh \
+	eza \
+	vi \
+	vim \
+	go \
+	rsync \
+	tree-sitter-cli \
+	unzip \
+	xclip \
+	wl-clipboard \
+	openssh \
+	navi \
+	fastfetch \
+	bc \
+	ncdu \
+	cargo \
+	man-db \
+	man-pages \
+	tealdeer \
+	vlc \
+	vlc-plugins-all \
+	lnav \
+	tree \
+	btop \
+	translate-shell \
+	tokei \
+	ffmpeg \
+	7zip \
+	ripgrep \
+	glow \
+	mpv \
+	chafa \
+	fd \
+	bluetui \
+	gource \
+	words \
+	detox \
+	git-lfs \
+	php composer jdk-openjdk julia \
+	wev \
+	nodejs \
+	npm \
+	nvm \
+	docker \
+	docker-compose \
+	docker-buildx \
+	postgresql \
+	dotnet-runtime \
+	dotnet-sdk \
+	aspnet-runtime \
+	aspnet-targeting-pack \
+	jq
 
-run_on_fedora <<'EOF'
-	sudo dnf install -y \
-		@development-tools \
-		wireplumber \
-		git \
-		gawk \
-		curl \
-		zsh \
-		eza \
-		vi \
-		vim \
-		golang \
-		rsync \
-		tree-sitter-cli \
-		unzip \
-		xclip \
-		wl-clipboard \
-		openssh \
-		navi \
-		fastfetch \
-		bc \
-		ncdu \
-		cargo \
-		man-db \
-		man-pages \
-		tealdeer \
-		vlc \
-		lnav \
-		tree \
-		btop \
-		translate-shell \
-		tokei \
-		7zip \
-		ripgrep \
-		glow \
-		mpv \
-		chafa \
-		fd-find \
-		gource \
-		words \
-		detox \
-		git-lfs \
-    php composer java-devel julia \
-		wev
+run_on_fedora sudo dnf install -y \
+	@development-tools \
+	wireplumber \
+	git \
+	gawk \
+	curl \
+	zsh \
+	eza \
+	vi \
+	vim \
+	golang \
+	rsync \
+	tree-sitter-cli \
+	unzip \
+	xclip \
+	wl-clipboard \
+	openssh \
+	navi \
+	fastfetch \
+	bc \
+	ncdu \
+	cargo \
+	man-db \
+	man-pages \
+	tealdeer \
+	vlc \
+	lnav \
+	tree \
+	btop \
+	translate-shell \
+	tokei \
+	7zip \
+	ripgrep \
+	glow \
+	mpv \
+	chafa \
+	fd-find \
+	gource \
+	words \
+	detox \
+	git-lfs \
+	php composer java-devel julia \
+	wev \
+	nodejs \
+	npm \
+	docker \
+	docker-compose \
+	postgresql \
+	jq
 
-	sudo dnf install -y --allowerasing ffmpeg
-EOF
+run_on_fedora sudo dnf install -y --allowerasing \
+	ffmpeg
 
 tldr --update
 
