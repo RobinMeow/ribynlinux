d4600e8f70b8d6b90865b86a55b09585dfc81959
Ribyn <robinmeow97@gmail.com>
2026-08-12 23:12:03 +0200

    nvim: revert hl.on_yank new one has different behavior

--- apps/nvim/config/lua/highlight-on-yank.lua
+++ apps/nvim/config/lua/highlight-on-yank.lua
@@ -1,8 +1,10 @@
---  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("highlight-on-yank", { clear = true }),
   callback = function()
-    vim.hl.hl_op()
+    vim.hl.on_yank()
+    -- NOTE: hl_op throws often. Ill wait for kickstart or neovim to fix it.
+    -- kickstart also still uses on_yank, for this reason prolly
+    -- vim.hl.hl_op()
   end,
 })
