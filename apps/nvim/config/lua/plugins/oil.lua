b0b2e2b418b69e70dacc6c4dc719b1ab68fb77b6
Ribyn <robinmeow97@gmail.com>
2026-08-13 12:04:07 +0200

    nvim: remove C-h keymap from oil

--- apps/nvim/config/lua/plugins/oil.lua
+++ apps/nvim/config/lua/plugins/oil.lua
@@ -3,13 +3,14 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    -- dependencies = {
-    -- 	{ "nvim-mini/mini.icons", opts = {} }
-    -- },
+    opts = {
+      keymaps = {
+        -- remove
+        ["<C-h>"] = false,
+      },
+    },
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations
     lazy = false,
   },
 }
