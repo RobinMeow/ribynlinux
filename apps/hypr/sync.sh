dfb33516f9d01a003aaf608b39a2ba14ce4c0f7a
Ribyn <robinmeow97@gmail.com>
2026-08-13 22:43:38 +0200

    hypr: improve eeyore lockscreen

--- apps/hypr/sync.sh
+++ apps/hypr/sync.sh
@@ -24,9 +24,15 @@ ribyn_rsync \
 ribyn_rsync "$RIBYN_ROOT/assets/images/wallpapers/hypr_chan.png" "$HOME/.config/hypr/"
 ribyn_rsync "$RIBYN_ROOT/assets/images/wallpapers/car-in-grass.jpg" "$HOME/.config/hypr/"
 
-# for hyprlock
-ribyn_rsync "$RIBYN_ROOT/assets/images/eeyore.png" "$HOME/.config/hypr/"
 ribyn_rsync "$RIBYN_ROOT/apps/hypr/hyprbye.sh" "$HOME/.local/bin/hyprbye"
+
+# eeyore hyprlock
+mkdir -p "$HOME/.config/hypr/eeyore/"
+ribyn_rsync "$RIBYN_ROOT/assets/images/eeyore.png" "$HOME/.config/hypr/eeyore/eeyore.png"
+# didnt like 'em
+# ribyn_rsync "$RIBYN_ROOT/assets/images/eeyore-duck-balloon.jpg" "$HOME/.config/hypr/eeyore/"
+# ribyn_rsync "$RIBYN_ROOT/assets/images/eeyore-pink-ribbon.jpg" "$HOME/.config/hypr/eeyore/"
+# ribyn_rsync "$RIBYN_ROOT/assets/images/cherry-blossom-blue-sky-frog-eye-view.jpg" "$HOME/.config/hypr/eeyore/"
 
 hypr_running=${HYPRLAND_INSTANCE_SIGNATURE:-"not running"}
 if [[ $hypr_running == "not running" ]]; then
@@ -36,7 +42,7 @@ else
 	hyprctl reload
 fi
 
-# i-am-root
+# i-am-root hyprlock
 mkdir -p "$HOME/.config/hypr/i-am-root/"
 
 # background
