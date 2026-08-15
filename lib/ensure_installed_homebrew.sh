0963204d171bba3a825cb154ee9d04d0ec0f95f5
Ribyn <robinmeow97@gmail.com>
2026-08-15 15:28:14 +0200

    brew: allow retries on frist time install

--- lib/ensure_installed_homebrew.sh
+++ lib/ensure_installed_homebrew.sh
@@ -8,6 +8,11 @@ BREW_PREFIX="/home/linuxbrew/.linuxbrew"
 
 if [[ -d "$BREW_PREFIX" ]]; then
 	echo "skipping brew installed (already installed)"
+
+	if command -v brew >/dev/null 2>&1; then
+		# first time install / rerunning the script can happen here
+		eval "$("$BREW_PREFIX/bin/brew" shellenv)"
+	fi
 else
 	warn "hypocrites request sudo permission just to remove it again after calling their bin, just so their own script can remain \"secure\"."
 	sudo --validate
