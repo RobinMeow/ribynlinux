e411933e40d08e3f16667c9d856479c289e329f0
Ribyn <robinmeow97@gmail.com>
2026-08-15 22:31:10 +0200

    hypr: build from source hyprshutdown and toolkit

--- apps/hypr/build-hyprtoolkit-from-source.sh
+++ apps/hypr/build-hyprtoolkit-from-source.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 mkdir -p "$HOME/.cache/ribyn/hypr/"
-logfile="$HOME/.cache/ribyn/hypr/hyprshutdown-build-from-source.log"
+logfile="$HOME/.cache/ribyn/hypr/hyprtoolkit-build-from-source.log"
 # Append to file and print to terminal simultaneously
 # use --append flag if you want to append, instead of override
 exec > >(tee "$logfile") 2>&1
@@ -12,25 +12,25 @@ exec > >(tee "$logfile") 2>&1
 . "$RIBYN_ROOT/lib/run_on_distro.sh"
 
 if on_arch; then
-	error "ERROR: Use pacman on archlinux to install hyprshutdown. use pacman -S hyprshutdown instead."
+	error "ERROR: Use pacman on archlinux to install hyprtoolkit. use pacman -S hyprtoolkit instead."
 	exit 1
 elif on_fedora; then
-	# WARN: depends on hyprtoolkit
 	sudo dnf install --assumeyes \
 		cmake \
 		gcc \
-		gcc-c++
+		gcc-c++ \
+		iniparser-devel
 
 	mkdir -p "$HOME/.local/share/ribyn/hypr/"
 	(
-		dest="$HOME/.local/share/ribyn/hypr/hyprshutdown"
+		dest="$HOME/.local/share/ribyn/hypr/hyprtoolkit"
 		if [[ -d "$dest" ]]; then
-			info "hyprshutdown already installed. skipping."
+			info "hyprtoolkit already installed. skipping."
 		else
-			info "hyprshutdown building from source"
-			git clone "https://github.com/hyprwm/hyprshutdown.git" "$dest"
+			info "hyprtoolkit building from source"
+			git clone "https://github.com/hyprwm/hyprtoolkit.git" "$dest"
 			cd "$dest"
-			git checkout $RIBYN_HYPR_HYPRSHUTDOWN_GIT_REF
+			git checkout $RIBYN_HYPR_HYPRTOOLKIT_GIT_REF
 			cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
 			cmake --build ./build --config Release --target all -j$(nproc 2>/dev/null || getconf NPROCESSORS_CONF)
 			sudo cmake --install ./build
--- apps/hypr/install.sh
+++ apps/hypr/install.sh
@@ -53,6 +53,8 @@ elif on_fedora; then
 		qt6ct
 
 	"$RIBYN_ROOT/apps/hypr/build-hyprpicker-from-source.sh"
+	"$RIBYN_ROOT/apps/hypr/build-hyprtoolkit-from-source.sh"
+	"$RIBYN_ROOT/apps/hypr/build-hyprshutdown-from-source.sh"
 fi
 
 "$RIBYN_ROOT/apps/hypr/build-hyprmoncfg-from-source.sh"
