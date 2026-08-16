7fc9b1dc37abe296e8ab73e446ec925bd568e99b
Ribyn <robinmeow97@gmail.com>
2026-08-16 11:51:26 +0200

    change comment for tldr --update failed when gh is down

--- scripts/pm-core.sh
+++ scripts/pm-core.sh
@@ -115,11 +115,10 @@ run_on_fedora sudo dnf install -y --allowerasing \
 	ffmpeg
 
 if ! tldr --update; then
-	# NOTE: this one breaks my CI when github is down
 	# Error: Could not update cache
 	# Caused by:
 	#     Could not download tldr pages from https://github.com/tldr-pages/tldr/releases/latest/download//tldr-pages.en_US.zip: Err(Io(Custom { kind: UnexpectedEof, error: "Peer disconnected" }))
-	warn "[pm-core] tldr --update failed; tldr pages might not be available or out of date." >&2
+	warn "[pm-core] tldr --update failed; tldr pages might not be available or out of date. Usually becuase github is currently down. Just run it later." >&2
 	press_any_to_continue
 fi
 
