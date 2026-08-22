597502fe54d2b42d6e522e49e7fc17c28853904f
Ribyn <robinmeow97@gmail.com>
2026-08-22 22:19:22 +0200

    hypr: change colorscheme to fallen knight

--- apps/hypr/config/colors.lua
+++ apps/hypr/config/colors.lua
@@ -8,10 +8,31 @@ local gruvbox_orange = "#fe8019"
 local hypr_default_blue = "#33ccffee"
 local hypr_default_cyan_green = "#00ff99ee"
 
-m.primary = electric_purple
-m.secondary = vibrant_rose
-m.tertiary = monokai_orange
+-- fallen knight (hyprpicked)
+-- #C67E86
+-- #9F6178
+local flower_rose = "#E98984"
+local cherry_blossom = "#9C3337"
+local earth = "#865220"
+local sunrise = "#CB7E28"
+local silver_armor = "#C1B09D"
+local chainmail = "#3D3D3C"
+
+-- fallen knight (neon-version)
+-- need bright colors for this one,
+-- becuase they are primarily showing which window has focus on
+local flower_rose_neon = "#FF2A55"
+local cherry_blossom_neon = "#FF433C"
+local earth_neon = "#FF5E00"
+local sunrise_neon = "#FFAC00"
+local silver_armor_neon = "#eeeeee"
+
+m.primary = sunrise_neon
+m.secondary = earth_neon
+m.tertiary = flower_rose
 
 m.shaow = "#ee1a1a1a"
+
+m.inactive = chainmail
 
 return m
--- apps/hypr/config/general.lua
+++ apps/hypr/config/general.lua
@@ -4,18 +4,19 @@ function m.setup()
     -- https://wiki.hypr.land/Configuring/Basics/Variables/#general
     general = {
       border_size = 2, -- size of the border around windows
-      gaps_in = 5, -- gaps between windows (default: 5)
+      gaps_in = 10, -- gaps between windows (default: 5)
       gaps_out = 20, -- gaps between windows and montior edges (default: 20)
 
       col = {
         active_border = { -- active window border color
           colors = { -- gradient
-            require("colors").secondary,
             require("colors").primary,
+            require("colors").secondary,
+            -- require("colors").tertiary,
           },
           angle = 45,
         },
-        inactive_border = "#595959aa", -- inactive window border color
+        inactive_border = require("colors").inactive, -- inactive window border color
       },
 
       -- use with care: https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/
