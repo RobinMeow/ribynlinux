6940e3b38f71b1c4cc23e3b9f8cb4d14c3f675b3
Ribyn <robinmeow97@gmail.com>
2026-08-24 20:08:17 +0200

    wl-freeze: use source-manager

--- apps/wl-freeze/build-from-source.sh
+++ apps/wl-freeze/build-from-source.sh
@@ -20,30 +20,48 @@ elif on_fedora; then
 		libnotify
 fi
 
-mkdir -p "$HOME/.local/share/ribyn/"
-dest="$HOME/.local/share/ribyn/wl-freeze"
-if [[ -d "$dest" ]]; then
-	info "wl-freeze is already installed. Skipping."
-else
-	(
-		git clone --depth 1 "https://github.com/Zerodya/wl-freeze" "$dest"
-		cd "$dest"
+source "$RIBYN_ROOT/lib/source-manager.sh"
+export RIBYN_WL_FREEZE_GITREV=${RIBYN_WL_FREEZE_GITREV:-"v2.1.0"}
+check_source_state "wl-freeze" "$RIBYN_WL_FREEZE_GITREV"
 
-		# make wl-freeze available in the cli as wl-freeze
-		# if .local/bin is in your PATH
-		mkdir -p "$HOME/.local/bin/"
-		dest_bin="$HOME/.local/bin/wl-freeze"
-		if [[ -f "$dest_bin" ]]; then
-			info "wl-freeze: symlink for PATH already exist. skipping."
-		else
-			ln -s "$(pwd)/wl-freeze" "$dest_bin"
-		fi
+# TODO: move the info messages into the source-manager
+function build_and_install() {
+	# make wl-freeze available in the cli as wl-freeze
+	# if .local/bin is in your PATH
+	mkdir -p "$HOME/.local/bin/"
+	dest_bin="$HOME/.local/bin/wl-freeze"
+	if [[ -f "$dest_bin" ]]; then
+		info "wl-freeze: symlink for PATH already exist. skipping."
+	else
+		ln -s "$(pwd)/wl-freeze" "$dest_bin"
+	fi
 
-		dest_comp="/usr/share/zsh/site-functions/"
-		if [[ -f "$dest_comp/_wl-freeze" ]]; then
-			info "wl-freeze: symlink for autocompletions already exist. skipping."
-		else
-			sudo ln -s "$(pwd)/completions/zsh/_wl-freeze" "$dest_comp/_wl-freeze"
-		fi
-	)
+	dest_comp="/usr/share/zsh/site-functions/"
+	if [[ -f "$dest_comp/_wl-freeze" ]]; then
+		info "wl-freeze: symlink for autocompletions already exist. skipping."
+	else
+		sudo ln -s "$(pwd)/completions/zsh/_wl-freeze" "$dest_comp/_wl-freeze"
+	fi
+}
+
+giturl="https://github.com/Zerodya/wl-freeze"
+if [[ "$SOURCE_STATE" == "source n/a" ]]; then
+	info "[$SOURCE_NAME] initialising..."
+	init_source "$giturl"
+	info "[$SOURCE_NAME] installing..."
+	(cd "$SOURCE_DEST" && build_and_install)
+elif [[ "$SOURCE_STATE" == "gitrev equals" ]]; then
+	if command -v wl-freeze >/dev/null 2>&1; then
+		info "$SOURCE_NAME already installed. Skipping."
+	else
+		# edge case. means its already cloned, but build probably failed
+		clean_source
+		info "[$SOURCE_NAME] installing..."
+		(cd "$SOURCE_DEST" && build_and_install)
+	fi
+elif [[ "$SOURCE_STATE" == "gitrev differs" ]]; then
+	clean_source
+	update_source "$giturl"
+	info "[$SOURCE_NAME] updating..."
+	(cd "$SOURCE_DEST" && build_and_install)
 fi
