9185a62bcfc6928c4eb9b16071b05454699bbb99
Ribyn <robinmeow97@gmail.com>
2026-08-27 20:13:37 +0200

    mpvpaper: lock to git tag 1.9

--- lib/mpvpaper/env.sh
+++ lib/mpvpaper/env.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 export RIBYN_MPVPAPER_ENABLED=${RIBYN_MPVPAPER_ENABLED:-"yes"}
 
-# TODO: pick latest tag
-export RIBYN_MPVPAPER_GITREV=${RIBYN_MPVPAPER_GITREV:-"origin/master"}
+# WARN: does not use the v prefix as the others
+export RIBYN_MPVPAPER_GITREV=${RIBYN_MPVPAPER_GITREV:-"1.9"}
