6c1162adc376398aab18b19b0f36fd292ee3dbe5
Ribyn <robinmeow97@gmail.com>
2026-09-09 10:28:44 +0200

    nvim: improve organize imports description

--- lib/nvim/config/init.lua
+++ lib/nvim/config/init.lua
@@ -83,7 +83,7 @@ require("lazy").setup({
                 diagnostics = {},
               },
             })
-          end, "auto sort imports", { "n", "x" })
+          end, "organize imports (sort, remove unused)", { "n", "x" })
           map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
           map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
           map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition") -- To jump back, press <C-t>
