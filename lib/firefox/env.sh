172ca377f388e765602dc115c6849678b40e472f
Ribyn <robinmeow97@gmail.com>
2026-09-11 21:42:20 +0200

    firefox: set as default browser on install

---
+++ lib/firefox/env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export RIBYN_FIREFOX_SET_DEFAULT_BROWSER=${RIBYN_FIREFOX_SET_DEFAULT_BROWSER:-"yes"}
--- lib/firefox/install.sh
+++ lib/firefox/install.sh
@@ -3,10 +3,23 @@ set -euo pipefail
 
 source "$RIBYN_ROOT/core/run_on_distro.sh"
 source "$RIBYN_ROOT/core/utils.sh"
+source "$RIBYN_ROOT/lib/firefox/env.sh"
 info "installing firefox"
 
-run_on_arch sudo pacman -S --needed --noconfirm \
-	firefox
+if on_arch; then
+	sudo pacman -S --needed --noconfirm \
+		firefox
+elif on_fedora; then
+	sudo dnf install -y \
+		firefox
+else
+	error "distro not supported."
+	exit 1
+fi
 
-run_on_fedora sudo dnf install -y \
-	firefox
+if [[ "$RIBYN_FIREFOX_SET_DEFAULT_BROWSER" == "yes" ]]; then
+	default_browser=$(xdg-settings get default-web-browser)
+	if [[ "$default_browser" != "org.mozilla.firefox.desktop" ]]; then
+		xdg-settings set default-web-browser "org.mozilla.firefox.desktop"
+	fi
+fi
