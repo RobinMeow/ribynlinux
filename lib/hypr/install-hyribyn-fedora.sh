f4431f6e0978ef2c214ac6ca00bec480e01633bb
Ribyn <robinmeow97@gmail.com>
2026-09-13 18:11:30 +0200

    migrate to hyribyn

---
+++ lib/hypr/install-hyribyn-fedora.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+source "$RIBYN_ROOT/lib/hypr/env.sh"
+[[ "$RIBYN_HYPR_ENABLED" == "no" ]] && exit 0
+
+source "$RIBYN_ROOT/core/utils.sh"
+
+source "$RIBYN_ROOT/core/run_on_distro.sh"
+if ! on_fedora; then
+	error "run hyribyn only on fedora."
+	exit 1
+fi
+
+mkdir -p "$HOME/.local/state/ribyn/"
+logfile="$HOME/.local/state/ribyn/install-hyribyn-fedora.log"
+# Append to file and print to terminal simultaneously
+# use --append flag if you want to append, instead of override
+exec > >(tee "$logfile") 2>&1
+
+info "installing hyribyn-fedora"
+
+# hyribyn will be installed in ~/.local/share/ribyn/hyribyn
+HYRIBYN="$HOME/.local/share/ribyn/"
+SOURCERER_DEST="$HYRIBYN" source "$RIBYN_ROOT/core/sourcerer.sh"
+
+function hyribyn_installed() {
+	# never consider it to be installed.
+	# hyribyn is already idempotent and knows what it needs to run what not.
+	return 0
+}
+
+function hyribyn_build_and_install() {
+	# use fedora as target distro
+	export HYRIBYN_DISTRO="fedora"
+	# this is where hyribyn will clone all the git repos,
+	# build, install and update them.
+	# hyribyn will also look in here for its own git location
+	# unless you set HYRIBYN
+	export HYRIBYN_ROOT="$HOME/.local/share/ribyn"
+	# install all hyprland dependencies, hyprland itself,
+	# and hyprland runtime dependencies
+	"$HYRIBYN/hyprland/install-stack.sh"
+}
+
+source "$HYRIBYN/versions.sh"
+
+check_source_state \
+	"hyribyn" \
+	"$RIBYN_HYRIBYN_GITREV"
+
+source_git "https://github.com/RobinMeow/hyribyn.git"
